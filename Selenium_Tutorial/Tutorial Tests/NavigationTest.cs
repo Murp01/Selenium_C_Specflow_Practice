320b2565925e501ea2cce96c128d04394de67b26
Murp01 <pmurphy35@hotmail.com>
2019-05-27 22:36:44 +0100

    First upload

---
+++ Selenium_Tutorial/Tutorial Tests/NavigationTest.cs
@@ -0,0 +1,41 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Selenium_Tutorial.Tutorial_Tests
+{
+    
+    class NavigationTest
+    {
+        IWebDriver driver;
+
+        [SetUp]
+        public void OpenWebApp()
+        {
+            driver = new FirefoxDriver();
+            //driver.Url = "";
+            driver.Navigate().GoToUrl("https://demoqa.com");
+        }
+
+        [Test]
+        public void NavigateBackAndForward()
+        {
+            driver.FindElement(By.XPath("//a[contains(text(),'Sortable')]")).Click();
+            driver.Navigate().Back();
+            driver.Navigate().Forward();
+            driver.Navigate().GoToUrl("https://demoqa.com");
+            driver.Navigate().Refresh();
+        }
+
+        [TearDown]
+        public void CloseTest()
+        {
+            driver.Close();
+        }
+    }
+}
